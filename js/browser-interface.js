bf2a101f6beca16d6fc8a5c0765f076e798970e0
Josh Bertsche <jjjbertsche@gmail.com>
2016-05-26 16:24:10 -0700

    add 9 skies

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,53 +1,63 @@
-var mapsApiKey = require('./../.env').mapsApiKey;
 var GoogleMapsLoader = require('google-maps'); // only for common js environments
-GoogleMapsLoader.KEY = mapsApiKey;
+GoogleMapsLoader.KEY = require('./../.env').mapsApiKey;
 
+randRange = function(min, max) {
+  return Math.random() * (max - min) + min;
+};
+
+setRandomLocation = function(p) {
+  var svs = new google.maps.StreetViewService();
+  var radius = 100;
+  var latLng = {
+    lat: randRange(48.845257, 29.787404),
+    lng: randRange(-124.849915, -66.721404)
+  };
+  svs.getPanorama({
+    location: latLng,
+    radius: radius
+  }, function(data, status) {
+    if (status === google.maps.StreetViewStatus.OK) {
+      console.log(data);
+      p[0].setPano(data.location.pano);
+    } else {
+      setRandomLocation(p)
+      console.log("no street view");
+    }
+  });
+
+}
 
 $(function() {
-  var heading = 0;
-  var pitch = 90;
-
   GoogleMapsLoader.load(function(google) {
-    var pano = new google.maps.StreetViewPanorama(
-      document.getElementById('map'), {
-        pov: {
-          heading: heading,
-          pitch: pitch
-        },
-        disableDefaultUI: true
-      });
-    randomLocation();
-
-
-    setInterval(function() {
-      randomLocation();
-    }, 5000)
-
-    setInterval(function() {
-      heading += .1;
-      pano.setPov({
-        heading: heading,
-        pitch: pitch
-      });
-    }, 1)
-
-
-    function randomLocation() {
-      var svs = new google.maps.StreetViewService();
-      var latLng = new google.maps.LatLng(Math.random() * 180 - 90, Math.random() * 360 - 180);
-      svs.getPanorama({
-        location: latLng,
-        radius: 1000000
-      }, function(data, status) {
-        if (status === google.maps.StreetViewStatus.OK) {
-          console.log(data);
-          pano.setPano(data.location.pano);
-        } else {
-          console.log("no street view");
-          randomLocation();
-        }
-      });
+    var panos = [];
+    for (var i = 0; i < 9; i++) {
+      newPano = new google.maps.StreetViewPanorama(
+        document.getElementById('pano' + i), {
+          pov: {
+            heading: 0,
+            pitch: 90
+          },
+        });
+        setRandomLocation([newPano]);
+        panos.push(newPano);
     }
+    console.log(panos)
+
+    setInterval(function() {
+      panos.forEach(function(e) {
+        e.setPov({
+          heading: e.pov.heading +=1,
+          pitch: e.pov.pitch
+        });
+      })
+    }, 300)
+
+    setInterval(function() {
+      for (var i = 0; i < panos.length; i++) {
+        setRandomLocation([panos[i]]);
+        $("#pano" + i + " p").text(panos[i].location.description);
+      }
+    }, 10000)
 
   });
 });
